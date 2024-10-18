9a0d1207bf4938ab27dddeb46834a555b3431b53
Eduardo Affonso Zacarias <Eduardoaffonsoz@hotmail.com>
2024-10-18 18:04:22 -0300

    Routes/views and Static files

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// Parse the incoming request body
-// Install body-parser
+const router = require('./routes/admin');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const path = require('path');
+const rootDir = require('./util/path');
+
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-})
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {  
-  //Getting the body for the incoming request 
-  console.log(req.body);
-  res.redirect('/');
-})
-
-app.use('/', (req, res, next) => {
-  console.log("In another middleware");
-  res.send('<h1>Hello from express</h1>');
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 })
 
 app.listen(3000);
+
---
+++ routes/admin.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const path = require('path');
+
+const router = express.Router();
+
+const rootDir = require('../util/path')
+
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+})
+
+router.post('/add-product', (req, res, next) => {  
+  //Getting the body for the incoming request 
+  console.log(req.body);
+  res.redirect('/');
+})
+
+
+module.exports = router; 
